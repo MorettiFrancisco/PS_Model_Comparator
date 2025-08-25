a079d65580ad027faf890cb2d515d580b5d1a5aa
morettiFrancisco <franmoretti02@hotmail.com>
2025-08-25 18:54:57 -0300

    refactor: Update frontend configuration and remove unused Nginx setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://fastapi_app:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
